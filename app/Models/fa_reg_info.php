9db156026045de9f54df6f552108a99d27125811
mohadese zandi <mohadesehzandix@gmail.com>
2025-06-01 11:44:33 +0330

    completed

---
+++ app/Models/fa_reg_info.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class fa_reg_info extends Model
+{
+    //
+}
---
+++ app/Models/mo_reg_info.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mo_reg_info extends Model
+{
+    //
+}
